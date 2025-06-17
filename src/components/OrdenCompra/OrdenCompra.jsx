fda44d95fd3d07fe59d5b25c6778e0f1a92c234c
JorgeRestrepo9 <zarta384@gmail.com>
2025-06-17 12:04:46 -0500

    MAS INTENSO QUE NOVIO FEO

--- src/components/OrdenCompra/OrdenCompra.jsx
+++ src/components/OrdenCompra/OrdenCompra.jsx
@@ -53,7 +53,7 @@ export default function OrdenCompra() {
       .then(res => setArticuloItems(res.data))
       .catch(() => setArticuloItems([]));
   };
-//cANSON
+//MAS INTENSO QUE NOVIO FEO
   useEffect(() => { reloadData(); }, [paginationModel, sortModel]);
 
   useEffect(() => {
